969a38a7bcb39b40acf531ecedf66cf1a772b70f
Harsh30303 <119389894+Harsh30303@users.noreply.github.com>
2024-07-16 20:04:19 +0000

    added variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+var accountID = "1234"
+const accountName = "Harsh"
+let accountPassword = "111"
+accountCity = "Delhi"
+let accountState
+
+//Note: We should prefer let over var, because of issues in block scope and functional scope
+
+
+accountID="000"
+accountPassword="hello"
+accountCity="gurgaon"
+
+console.table([accountID,accountName,accountPassword,accountCity, accountState])
